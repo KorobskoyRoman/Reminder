e44a1157b10dafb35879bceabab1c101151f4478
KorobskoyRoman <shopone023@gmail.com>
2023-01-09 19:53:43 +0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Roman Korobskoy on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
