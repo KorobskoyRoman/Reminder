71f4e9cbfdeef89283d9fd2fe9992cf646d95752
KorobskoyRoman <shopone023@gmail.com>
2023-01-09 20:42:21 +0300

    feat: Ext for UIFont

--- Reminder/Extensions/UIFont+.swift
+++ Reminder/Extensions/UIFont+.swift
@@ -5,4 +5,14 @@
 //  Created by Roman Korobskoy on 09.01.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    class var navBarTitle: UIFont {
+        return UIFont.systemFont(ofSize: 21.0, weight: .bold)
+    }
+
+    class var navBarLargeTitle: UIFont {
+        return UIFont.systemFont(ofSize: 35.0, weight: .bold)
+    }
+}
