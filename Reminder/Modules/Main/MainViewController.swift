86d1e55953ea062eb3316096d2d9de49fa5e56f9
KorobskoyRoman <shopone023@gmail.com>
2023-01-10 17:29:05 +0300

    feat: Update vc

--- Reminder/Modules/Main/MainViewController.swift
+++ Reminder/Modules/Main/MainViewController.swift
@@ -8,10 +8,86 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+
+    var dataSource: DataSource?
+
+    private lazy var collectionView = UICollectionView(
+        frame: view.bounds,
+        collectionViewLayout: listLayout()
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        navigationController?.title = "First"
+        view.backgroundColor = .black
+        setupCollectionView()
+    }
+
+    private func setupCollectionView() {
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .black
+        setupDataSource()
     }
 }
 
+extension MainViewController {
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+
+    private func setupDataSource() {
+        let cellReg = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+
+        var snapshot = Snapshot()
+
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellReg,
+                for: indexPath,
+                item: itemIdentifier)
+        }
+
+        snapshot.appendSections([0])
+        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+        dataSource?.apply(snapshot)
+
+        collectionView.dataSource = dataSource
+    }
+
+    private func cellRegistrationHandler(
+        cell: UICollectionViewListCell,
+        indexPath: IndexPath,
+        id: String
+    ) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+
+        var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
+        doneButtonConfiguration.tintColor = .lightGray.withAlphaComponent(0.8)
+        cell.accessories = [
+            .customView(configuration: doneButtonConfiguration),
+            .disclosureIndicator(displayed: .always)
+        ]
+
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = .darkGray
+        cell.backgroundConfiguration = backgroundConfiguration
+    }
+
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
+    }
+}
