463f00200cbf85013873c88f4db8636e27a8f52c
KorobskoyRoman <shopone023@gmail.com>
2023-01-11 21:01:50 +0300

    feat: New ext for index

--- Reminder/Extensions/Array+Reminder.swift
+++ Reminder/Extensions/Array+Reminder.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension Array where Element == Reminder {
+    func indexOfReminder(with id: Reminder.ID) -> Self.Index {
+        guard let index = firstIndex(where: { $0.id == id }) else {
+            fatalError("No index yet!")
+        }
+        return index
+    }
+}
