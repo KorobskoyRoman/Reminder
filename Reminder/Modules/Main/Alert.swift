46d9cbdfd6933296b9d197d18fba30043edacca4
KorobskoyRoman <shopone023@gmail.com>
2023-01-16 23:26:18 +0300

    feat: Update alert

--- Reminder/Modules/Main/Alert.swift
+++ Reminder/Modules/Main/Alert.swift
@@ -5,4 +5,23 @@
 //  Created by Roman Korobskoy on 16.01.2023.
 //
 
-import Foundation
+import UIKit
+
+final class Alert {
+    static let shared = Alert()
+
+    func showError(vc: UIViewController, _ error: Error) {
+        let alertTitle = NSLocalizedString("Error",
+                                           comment: "Error alert title")
+        let alert = UIAlertController(title: alertTitle,
+                                      message: error.localizedDescription,
+                                      preferredStyle: .alert)
+        let actionTitle = NSLocalizedString("OK",
+                                            comment: "Alert OK button title")
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default) { _ in
+            vc.dismiss(animated: true)
+        })
+
+        vc.present(alert, animated: true)
+    }
+}
