e22c68d836b81bdb3ac3f88953188d02ecdc4ec6
KorobskoyRoman <shopone023@gmail.com>
2023-01-17 00:55:29 +0300

    feat: Ext init

---
+++ Reminder/Models/EventKit/Reminder+EKReminder.swift
@@ -0,0 +1,22 @@
+//
+//  Reminder+EKReminder.swift
+//  Reminder
+//
+//  Created by Roman Korobskoy on 16.01.2023.
+//
+
+import Foundation
+import EventKit
+
+extension Reminder {
+    init(with ekReminder: EKReminder) throws {
+        guard let dueDate = ekReminder.alarms?.first?.absoluteDate else {
+            throw ReminderError.reminderHasNoDueDate
+        }
+        id = ekReminder.calendarItemIdentifier
+        title = ekReminder.title
+        self.dueDate = dueDate
+        notes = ekReminder.notes
+        isComplete = ekReminder.isCompleted
+    }
+}
